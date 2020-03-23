8db70d9dc34fbc55803f2c71ea2dff2172f99ee7
Stepan47 <57790730+Stepan47@users.noreply.github.com>
2020-03-23 21:38:35 +0400

    new files

---
+++ mikrob_3d/Assets/scripts/scriptBots/BaseScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseScript : MonoBehaviour
+{
+    public GameObject MyCube;
+    private string[] NameBots = new string[300];
+    private Dictionary<string, int[]> BotsCommands = new Dictionary<string, int[]>(300);
+    private int index;
+    private int index_command = 0;
+    void Start()
+    {
+    	index = 0;
+        
+    }
+    // Здесь идет проход по всем ботам и выполнение команд
+    void Commands_start(){
+        for (int i=0;i<index;i++){
+            float bot_command = BotsCommands[NameBots[i]][index_command];
+            MyCube = GameObject.Find(NameBots[i]);
+            if (bot_command == 0f){
+                MyCube.transform.position = new Vector3(MyCube.transform.position.x,MyCube.transform.position.y,MyCube.transform.position.z + 0.01f);
+
+            }
+            if (bot_command  == 1f){
+                MyCube.transform.position = new Vector3(MyCube.transform.position.x,MyCube.transform.position.y,MyCube.transform.position.z - 0.01f);
+
+            }
+            if (bot_command  == 2f){
+                MyCube.transform.position = new Vector3(MyCube.transform.position.x-0.01f,MyCube.transform.position.y,MyCube.transform.position.z);
+
+            }
+            if (bot_command  == 3f){
+                MyCube.transform.position = new Vector3(MyCube.transform.position.x+0.01f,MyCube.transform.position.y,MyCube.transform.position.z);
+
+            }
+            else{
+                MyCube.transform.position = new Vector3(MyCube.transform.position.x,MyCube.transform.position.y+0.01f,MyCube.transform.position.z);
+            }
+            
+
+        }
+        if (index_command >= 1){
+            index_command = 0;
+        }
+        index_command++;
+    }
+    
+    void Update() 
+    {
+    	if (Input.GetKeyDown(KeyCode.N)){
+    		MyCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+    		MyCube.AddComponent<Rigidbody>();
+    		MyCube.transform.position = new Vector3(200f, 25f, 130f);
+    		MyCube.name = "Bot"+index.ToString();
+    		NameBots[index] = "Bot"+index.ToString();
+            int[] list = new int[2];
+            for (int i=0;i<list.GetLength(0);i++){
+                list[i] = Random.Range(0,4);
+            }
+            BotsCommands.Add("Bot"+index.ToString(),list);
+    		index++;
+    	}
+        Commands_start();
+    }
+}
---
+++ mikrob_3d/Assets/scripts/scriptsViews/CameraMove.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveY; 
+    public float moveX;
+    public float moveX_for_move;
+    public float moveZ;
+    public float moveY_for_move;
+    private bool is_moveZW;
+    private bool is_moveZS;
+    private bool is_moveXA;
+    private bool is_moveXD;
+    private bool is_moveYSHIFT;
+    private bool is_moveYSPACE;
+    private bool is_moveX;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	moveY = Input.GetAxis("Mouse Y");
+    	moveX = Input.GetAxis("Mouse X");
+    	//движение на кнопку "W"
+    	if (Input.GetKeyDown(KeyCode.W)){
+    		is_moveZW = true;
+    	}
+    	if (Input.GetKeyUp(KeyCode.W)){
+    		is_moveZW = false;
+    	}
+    	if (is_moveZW){
+    		moveZ += 0.5f;
+    	}
+        //движение на кнопку "S"
+    	if (Input.GetKeyDown(KeyCode.S)){
+    		is_moveZS = true;
+    	}
+    	if (Input.GetKeyUp(KeyCode.S)){
+    		is_moveZS = false;
+    	}
+    	if (is_moveZS){
+    		moveZ += -0.5f;
+    	}
+        //движение на кнопку "D"
+        if (Input.GetKeyDown(KeyCode.D)){
+            is_moveXD = true;
+        }
+        if (Input.GetKeyUp(KeyCode.D)){
+            is_moveXD = false;
+        }
+        if (is_moveXD){
+            moveX_for_move += 0.5f;
+        }
+        //движение на кнопку "A"
+        if (Input.GetKeyDown(KeyCode.A)){
+            is_moveXA = true;
+        }
+        if (Input.GetKeyUp(KeyCode.A)){
+            is_moveXA = false;
+        }
+        if (is_moveXA){
+            moveX_for_move += -0.5f;
+        }
+        //движение на кнопку "SHIFT"
+        if (Input.GetKeyDown(KeyCode.LeftShift)){
+            is_moveYSHIFT = true;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift)){
+            is_moveYSHIFT = false;
+        }
+        if (is_moveYSHIFT){
+            moveY_for_move += -0.5f;
+        }
+        //движение на кнопку "Space"
+        if (Input.GetKeyDown(KeyCode.Space)){
+            is_moveYSPACE = true;
+        }
+        if (Input.GetKeyUp(KeyCode.Space)){
+            is_moveYSPACE = false;
+        }
+        if (is_moveYSPACE){
+            moveY_for_move += 0.5f;
+        }
+    	transform.position = new Vector3(transform.position.x + moveX_for_move, transform.position.y + moveY_for_move,transform.position.z + moveZ);
+        transform.Rotate((moveY*-1) * 12f,moveX * 12f,0f);
+        moveX_for_move = 0f;
+        moveY_for_move = 0f;
+        moveZ = 0f;
+    }
+}
